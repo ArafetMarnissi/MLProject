e0fc040b924acc7121298800a2cf2872dfd8f051
arafet <marnissiarafet@gmail.com>
2024-06-12 15:01:08 +0100

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
 import sys
-from logger import logging
+import os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(project_root)
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,10 +18,12 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
-# if __name__=="__main__":
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)
+    
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
  
